71caded1b00ee7464036c3f63e622478db73322b
Rainer <rainer@software-architects.at>
2020-10-01 14:17:25 +0200

    Closes #3, closes #4

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as chalk from 'chalk';
 import * as rl from 'readline-sync';
+import * as math from './math';
 
 console.log(chalk.bgRed.white('Welcome to FancyPancyCalc!'));
 
@@ -9,6 +10,26 @@ console.log('Next one please:');
 const n2 = parseInt(rl.question());
 
 while (true) {
-    console.log(chalk.red('More great features will follow here'));
-    rl.question();
+    console.log('Press ' + chalk.bgYellow.black('+') + ' for adding');
+    console.log('Press ' + chalk.bgYellow.black('-') + ' to subtract');
+    console.log('Press ' + chalk.bgYellow.black('q') + ' to quit');
+    const selection = rl.question();
+
+    let result: number;
+    switch (selection) {
+        case 'q':
+            process.exit(0);
+        case '+':
+            result = math.add(n1, n2);
+            console.log(`The result of ${n1} + ${n2} is ${chalk.bgGreenBright.black(result)}.`);
+            break;
+        case '-':
+            break;
+        default:
+            console.log(chalk.red('Invalid selection, please try again!'));
+    }
+
+    if (selection === 'q') {
+        break;
+    }
 }
